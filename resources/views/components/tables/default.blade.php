1a0461be7ef35f43484768af9bf949de8b113a66
Amstrong1 <lewisamstrongh@gmail.com>
2024-02-14 22:34:07 +0100

    eq<ra

--- resources/views/components/tables/default.blade.php
+++ resources/views/components/tables/default.blade.php
@@ -1,25 +1,24 @@
-<div class="flex flex-col items-stretch w-full overflow-hidden rounded-lg shadow-xs border dark:border-gray-700">
+<div class="flex flex-col items-stretch w-full overflow-hidden rounded-lg shadow-xs border">
     <div class="w-full overflow-x-auto">
         <div class="flex justify-center items-center p-4 border-b table-search-container">
             <label for="table-search" class="sr-only">Rechercher</label>
             <div class="relative">
                 <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                    <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor"
-                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
                             d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
                             clip-rule="evenodd"></path>
                     </svg>
                 </div>
                 <input type="text" id="custom-search-input"
-                    class="block p-2 pl-10 w-80 text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="block p-2 pl-10 w-80 text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                     placeholder="Rechercher dans la liste">
             </div>
         </div>
         <table class="w-full whitespace-no-wrap" id="datas-table-buttons" style="width: 100% !important">
             <thead>
-                <tr
-                    class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b bg-gray-50">
                     @foreach ($mattributes as $column => $title)
                         <th class="px-4 py-3 text-center ">{{ $title }}</th>
                     @endforeach
@@ -28,10 +27,10 @@
                     @endisset
                 </tr>
             </thead>
-            <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+            <tbody class="bg-white divide-y">
                 @if (count($resources) > 0)
                     @foreach ($resources as $resource)
-                        <tr class="text-gray-700 dark:text-gray-400">
+                        <tr class="text-gray-700">
                             @foreach ($mattributes as $column => $title)
                                 <td class="px-4 py-3 text-center">
                                     @if ($column == 'img' || $column == 'image' || $column == 'photo' || $column == 'logo')
@@ -83,7 +82,7 @@
                                         @foreach ($mactions as $action => $title)
                                             @if ($action == 'show')
                                                 <a href="{{ route($type . '.show', [$type => $resource->id]) }}"
-                                                    class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray">
+                                                    class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 rounded-lg focus:outline-none focus:shadow-outline-gray">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5"
                                                         viewBox="0 0 20 20" fill="currentColor">
                                                         <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
@@ -110,7 +109,7 @@
                                                     @method('DELETE')
                                                     @csrf
                                                     <button type="submit"
-                                                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-red-600  rounded-lg dark:text-red-600 focus:outline-none focus:shadow-outline-gray"
+                                                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-red-600 rounded-lg dark:text-red-600 focus:outline-none focus:shadow-outline-gray"
                                                         aria-label="Delete">
                                                         <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
                                                             viewBox="0 0 20 20">
@@ -137,7 +136,7 @@
         </table>
     </div>
     {{-- <div
-         class="grid px-4 py-3 text-center text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+         class="grid px-4 py-3 text-center text-xs font-semibold tracking-wide text-gray-500 uppercase border-t bg-gray-50">
         <!-- Pagination -->
         <span class="flex mt-2 py-3 sm:mt-auto sm:justify-center">
             {{ $resources->links('components.elements.pagination.default') }}
